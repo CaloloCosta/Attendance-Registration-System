812598980ad617b58184d825d047ac00a9e2e386
Fox <36572168+Fox520@users.noreply.github.com>
2019-10-09 21:38:18 +0200

    Update DB_schema.sql

--- backend/flaskr/DB_schema.sql
+++ backend/flaskr/DB_schema.sql
@@ -1,32 +1,32 @@
 -- DROP TABLE IF EXISTS student;
 -- DROP TABLE IF EXISTS lecturer;
-DROP TABLE IF EXISTS attendance;
+-- DROP TABLE IF EXISTS attendance;
 -- DROP TABLE IF EXISTS markAttendance;
 -- DROP TABLE IF EXISTS user;
 
--- CREATE TABLE user IF NOT EXISTS (
---     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
---     username TEXT NOT NULL,
---     password TEXT NOT NULL
--- );
+CREATE TABLE user (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL
+);
 
 
 
--- CREATE TABLE student (
---     studentNumber TEXT UNIQUE PRIMARY KEY,
---     surname TEXT NOT NULL,
---     initials TEXT NOT NULL,
---     programme TEXT NOT NULL,
---     mode_of_study TEXT NOT NULL,
---     password TEXT NULL
--- );
+CREATE TABLE student (
+    studentNumber TEXT UNIQUE PRIMARY KEY,
+    surname TEXT NOT NULL,
+    initials TEXT NOT NULL,
+    programme TEXT NOT NULL,
+    mode_of_study TEXT NOT NULL,
+    password TEXT NULL
+);
 
--- CREATE TABLE lecturer (
---     lecturerId TEXT UNIQUE NOT NULL PRIMARY KEY,
---     surname TEXT NOT NULL,
---     initials TEXT NOT NULL,
---     password TEXT NULL
--- );
+CREATE TABLE lecturer (
+    lecturerId TEXT UNIQUE NOT NULL PRIMARY KEY,
+    surname TEXT NOT NULL,
+    initials TEXT NOT NULL,
+    password TEXT NULL
+);
 
 
 
@@ -37,10 +37,10 @@ CREATE TABLE attendance (
     mode_of_study TEXT NOT NULL
 );
 
--- CREATE TABLE markAttendance (
---      attendanceId INTEGER NOT NULL,
---      studentNumber TEXT NOT NULL,
---      present BOOLEAN NOT NULL,
---      FOREIGN KEY (studentNumber) REFERENCES student (studentNumber),
---      FOREIGN key (attendanceId) REFERENCES attendance (attendanceId)
--- );
+CREATE TABLE markAttendance (
+     attendanceId INTEGER NOT NULL,
+     studentNumber TEXT NOT NULL,
+     present BOOLEAN NOT NULL,
+     FOREIGN KEY (studentNumber) REFERENCES student (studentNumber),
+     FOREIGN key (attendanceId) REFERENCES attendance (attendanceId)
+);
